1b03a3b3a47fd45d7a3693bf1f59e9dd256e176e
kerkkerk12 <chittawan_wetchaya2hotmail.com>
2022-06-06 17:01:33 +0700

    module_test

---
+++ Modules/Test/Http/Controllers/TestController.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace Modules\Test\Http\Controllers;
+
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class TestController extends Controller
+{
+    public function __construct()
+    {
+        // Page Title
+        $this->module_title = 'TestCase';
+
+        // module name
+        $this->module_name = 'test';
+
+        // directory path of the module
+        $this->module_path = 'test';
+
+        // module icon
+        $this->module_icon = 'c-icon fas fa-bell';
+
+        // module model name, path
+        $this->module_model = "App\Models\Test";
+    }
+    /**
+     * Display a listing of the resource.
+     * @return Renderable
+     */
+    public function index()
+    {
+        $module_title = $this->module_title;
+        $module_name = $this->module_name;
+        $module_path = $this->module_path;
+        $module_icon = $this->module_icon;
+        $module_model = $this->module_model;
+        $module_name_singular = Str::singular($module_name);
+
+        $module_action = 'List';
+
+        $$module_name = auth()->user()->notifications()->paginate();
+        $unread_notifications_count = auth()->user()->unreadNotifications()->count();
+
+        Log::info(label_case($module_title.' '.$module_action).' | User:'.Auth::user()->name.'(ID:'.Auth::user()->id.')');
+
+        return view(
+            "backend.$module_path.index",
+            compact('module_title', 'module_name', "$module_name", 'module_path', 'module_icon', 'module_action', 'module_name_singular', 'unread_notifications_count')
+        );
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     * @return Renderable
+     */
+    public function create()
+    {
+        return view('test::create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * @param Request $request
+     * @return Renderable
+     */
+    public function store(Request $request)
+    {
+        $module_title = $this->module_title;
+        $module_name = $this->module_name;
+        $module_path = $this->module_path;
+        $module_icon = $this->module_icon;
+        $module_model = $this->module_model;
+        $module_name_singular = Str::singular($module_name);
+
+        $module_action = 'Store';
+
+        $validatedData = $request->validate([
+            'name' => 'required'
+        ]);
+
+        $$module_name_singular = $module_model::create($request->except('image'));
+
+        if ($request->image) {
+            $media = $$module_name_singular->addMedia($request->file('image'))->toMediaCollection($module_name);
+            $$module_name_singular->image = $media->getUrl();
+            $$module_name_singular->save();
+        }
+
+        flash(icon().' '.Str::singular($module_title)."' Created.")->success()->important();
+
+        logUserAccess($module_title.' '.$module_action.' | Id: '.$$module_name_singular->id);
+
+        return redirect("admin/$module_name");
+    }
+
+    /**
+     * Show the specified resource.
+     * @param int $id
+     * @return Renderable
+     */
+    public function show($id)
+    {
+        $module_title = $this->module_title;
+        $module_name = $this->module_name;
+        $module_path = $this->module_path;
+        $module_icon = $this->module_icon;
+        $module_model = $this->module_model;
+        $module_name_singular = Str::singular($module_name);
+
+        $module_action = 'Show';
+
+        $$module_name_singular = $module_model::findOrFail($id);
+
+        $posts = $$module_name_singular->posts()->latest()->paginate();
+
+        logUserAccess($module_title.' '.$module_action.' | Id: '.$$module_name_singular->id);
+
+        return view(
+            "$module_path.$module_name.show",
+            compact('module_title', 'module_name', 'module_icon', 'module_name_singular', 'module_action', "$module_name_singular", 'posts')
+        );
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     * @param int $id
+     * @return Renderable
+     */
+    public function edit($id)
+    {
+        return view('test::edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * @param Request $request
+     * @param int $id
+     * @return Renderable
+     */
+    public function update(Request $request, $id)
+    {
+        $module_title = $this->module_title;
+        $module_name = $this->module_name;
+        $module_path = $this->module_path;
+        $module_icon = $this->module_icon;
+        $module_model = $this->module_model;
+        $module_name_singular = Str::singular($module_name);
+
+        $module_action = 'Update';
+
+        $validatedData = $request->validate([
+            'name' => 'required|max:191|unique:'.$module_model.',name,'.$id,
+            'slug' => 'nullable|max:191|unique:'.$module_model.',slug,'.$id,
+        ]);
+
+        $$module_name_singular = $module_model::findOrFail($id);
+
+        $$module_name_singular->update($request->except('image', 'image_remove'));
+
+        // Image
+        if ($request->hasFile('image')) {
+            if ($$module_name_singular->getMedia($module_name)->first()) {
+                $$module_name_singular->getMedia($module_name)->first()->delete();
+            }
+            $media = $$module_name_singular->addMedia($request->file('image'))->toMediaCollection($module_name);
+
+            $$module_name_singular->image = $media->getUrl();
+
+            $$module_name_singular->save();
+        }
+        if ($request->image_remove == 'image_remove') {
+            if ($$module_name_singular->getMedia($module_name)->first()) {
+                $$module_name_singular->getMedia($module_name)->first()->delete();
+
+                $$module_name_singular->image = '';
+
+                $$module_name_singular->save();
+            }
+        }
+
+        flash(icon().' '.Str::singular($module_title)."' Updated Successfully")->success()->important();
+
+        logUserAccess($module_title.' '.$module_action.' | Id: '.$$module_name_singular->id);
+
+        return redirect()->route('backend.tags.show', $$module_name_singular->id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * @param int $id
+     * @return Renderable
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
