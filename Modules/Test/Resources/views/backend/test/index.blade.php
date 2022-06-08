8e75031adf39e4f27b346180745b566595f41eff
LAPTOP-HOQHD6RT\User <koonmos1@gmail.com>
2022-06-08 09:49:18 +0700

    test index

--- Modules/Test/Resources/views/backend/test/index.blade.php
+++ Modules/Test/Resources/views/backend/test/index.blade.php
@@ -13,7 +13,7 @@
     <div class="card-body">
 
         <x-backend.section-header>
-            <i class="{{ $module_icon }}"></i> {{ __($module_title) }} <small class="text-muted">{{ __($module_action) }}</small>
+            {{ __($module_title) }} <small class="text-muted">{{ __($module_action) }}</small>
             <x-slot name="subtitle">
                 @lang(":module_name Management Dashboard", ['module_name'=>Str::title($module_name)])
             </x-slot>
